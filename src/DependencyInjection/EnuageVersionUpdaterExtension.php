869d223832755d1df87e152c6aec503c1962bb11
Serghei Niculaev <spam312sn@gmail.com>
2018-12-20 02:19:26 +0200

    Move files in according to Symfony guidelines

--- src/DependencyInjection/EnuageVersionUpdaterExtension.php
+++ src/DependencyInjection/EnuageVersionUpdaterExtension.php
@@ -12,10 +12,10 @@
 
 namespace Enuage\VersionUpdaterBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * Class EnuageVersionUpdaterExtension
