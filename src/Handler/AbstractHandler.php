8d90858fa4cfab37677647566d9995edf94485d3
Serghei Niculaev <spam312sn@gmail.com>
2019-07-01 01:13:15 +0300

    1, WIP: Add handlers

---
+++ src/Handler/AbstractHandler.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * AbstractHandler
+ *
+ * Created at 2019-06-30 11:39 PM
+ *
+ * @author Serghei Niculaev <spam312sn@gmail.com>
+ * @license GNU GPLv3 <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ *
+ * This file is a part of éNuage version updater command
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Enuage\VersionUpdaterBundle\Handler;
+
+/**
+ * Class AbstractHandler
+ *
+ * @author Serghei Niculaev <spam312sn@gmail.com>
+ */
+abstract class AbstractHandler
+{
+    // TODO
+}
